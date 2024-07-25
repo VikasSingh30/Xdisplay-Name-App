7254568b2a5153493311a96297e4f29f23abe534
Vikas <singhvikas.sv30@gmail.com>
2024-07-25 14:01:50 +0000

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function displayFullName(event) {
+    event.preventDefault();
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    if (firstName && lastName) {
+        document.getElementById("fullNameDisplay").innerText = "Full Name: " + firstName + " " + lastName;
+    }
+}
