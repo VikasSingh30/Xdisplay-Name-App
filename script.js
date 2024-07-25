9fc0bd79f70d7b5d15ec967997a98699096cba2c
Vikas <singhvikas.sv30@gmail.com>
2024-07-25 22:51:41 +0000

    third commit

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-// function displayFullName(event) {
-//     event.preventDefault();
-//     var firstName = document.getElementById("firstName").value;
-//     var lastName = document.getElementById("lastName").value;
-//     if (firstName && lastName) {
-//         document.getElementById("fullNameDisplay").innerText = "Full Name: " + firstName + " " + lastName;
-//     }else {
-//         document.getElementById("fullNameDisplay").innerText = "";
-//     }
-// }
 function displayFullName(event) {
     event.preventDefault();
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
-    
-    console.log("First Name:", firstName);
-    console.log("Last Name:", lastName);
-
     if (firstName && lastName) {
         document.getElementById("fullNameDisplay").innerText = "Full Name: " + firstName + " " + lastName;
-    } else {
+    }else {
         document.getElementById("fullNameDisplay").innerText = "";
     }
-    
-    // Log the result to see what is being displayed
-    console.log("Displayed Text:", document.getElementById("fullNameDisplay").innerText);
 }
+
