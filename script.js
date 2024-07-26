3e17e6ca8974d66d7a095c2b43ec3a71ba58384f
Vikas <160794716+VikasSingh30@users.noreply.github.com>
2024-07-26 23:52:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function displayFullName(event) {
     event.preventDefault();
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
-    if (firstName && lastName) {
-        document.getElementById("fullNameDisplay").innerText = "Full Name: " + firstName + " " + lastName;
-    }else {
-        document.getElementById("fullNameDisplay").innerText = "";
-    }
+    document.getElementById("fullNameDisplay").innerText = "Full Name: " + firstName + " " + lastName;
+    // if (firstName && lastName) {
+    //     document.getElementById("fullNameDisplay").innerText = "Full Name: " + firstName + " " + lastName;
+    // }else {
+    //     document.getElementById("fullNameDisplay").innerText = "";
+    // }
 }
 
