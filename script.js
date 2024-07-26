1340c2a55419a6670c199f767a9b7665dc9559b5
Vikas <160794716+VikasSingh30@users.noreply.github.com>
2024-07-26 23:56:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ function displayFullName(event) {
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
     document.getElementById("fullNameDisplay").innerText = "Full Name: " + firstName + " " + lastName;
-    // if (firstName && lastName) {
-    //     document.getElementById("fullNameDisplay").innerText = "Full Name: " + firstName + " " + lastName;
-    // }else {
-    //     document.getElementById("fullNameDisplay").innerText = "";
-    // }
+
 }
 
