b41ee88799b7ca38d8c8fa1c40cddd40dc1df9cf
Vikas <160794716+VikasSingh30@users.noreply.github.com>
2024-07-27 17:37:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,7 @@ function displayFullName(event) {
     event.preventDefault();
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
-    // document.getElementById("fullNameDisplay").innerText = "Full Name: " + firstName + " " + lastName;
-     if (!firstName || !lastName) {
-                document.getElementById("fullNameDisplay").innerText = "Please enter both first and last names.";
-            } else {
-                document.getElementById("fullNameDisplay").innerText = "Full Name: " + firstName + " " + lastName;
-            }
+    document.getElementById("fullNameDisplay").innerText = "Full Name: " + firstName + " " + lastName;
 
 }
 
